efd7118a8be12a9dc1f8fc56d062eee580ee50b6
airi-i1998 <airi.inoue@generosity.co.jp>
2024-10-14 20:00:08 +0900

    add: calculation result

---
+++ app.js
@@ -0,0 +1,9 @@
+const userInput = document.getElementById('user-number')
+const addBtn = document.getElementById('addBtn')
+const subtracBtn = document.getElementById('subtracBtn')
+const multipleBtn = document.getElementById("multipleBtn")
+const divideBtn = document.getElementById("divideBtn")
+const currentResult = document.getElementById('current-result')
+
+let result = 20
+currentResult.textContent = result
